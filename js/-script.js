4f38041bef3520f7dc720c6e761df4ab1f512158
krukov_ag <krukovag@ya.ru>
2021-11-01 17:01:15 +0300

    initial

---
+++ js/-script.js
@@ -0,0 +1,201 @@
+/*jshint jquery:true */
+/*global $:true */
+
+var $ = jQuery.noConflict();
+
+$(document).ready(function($) {
+	"use strict";
+
+	/*-------------------------------------------------*/
+	/* =  portfolio isotope
+	/*-------------------------------------------------*/
+
+	var winDow = $(window);
+		// Needed variables
+		var $container=$('.portfolio-box');
+		var $filter=$('.filter');
+
+		try{
+			$container.imagesLoaded( function(){
+				$container.show();
+				$container.isotope({
+					filter:'*',
+					layoutMode:'masonry',
+					animationOptions:{
+						duration:750,
+						easing:'linear'
+					}
+				});
+			});
+		} catch(err) {
+		}
+
+		winDow.bind('resize', function(){
+			var selector = $filter.find('a.active').attr('data-filter');
+
+			try {
+				$container.isotope({ 
+					filter	: selector,
+					animationOptions: {
+						duration: 750,
+						easing	: 'linear',
+						queue	: false,
+					}
+				});
+			} catch(err) {
+			}
+			return false;
+		});
+		
+		// Isotope Filter 
+		$filter.find('a').click(function(){
+			var selector = $(this).attr('data-filter');
+
+			try {
+				$container.isotope({ 
+					filter	: selector,
+					animationOptions: {
+						duration: 750,
+						easing	: 'linear',
+						queue	: false,
+					}
+				});
+			} catch(err) {
+
+			}
+			return false;
+		});
+
+
+	/*-------------------------------------------------*/
+	/* =   filter portfolio
+	/*-------------------------------------------------*/
+
+	var filterlink = $('a.category'),
+		ulFilter = $('ul.filter');
+
+		filterlink.on('click', function(e){
+			e.preventDefault();
+
+			if( !ulFilter.hasClass('active') ) {
+				ulFilter.addClass('active');
+				ulFilter.slideDown(300);
+			} else {
+				ulFilter.removeClass('active');
+				ulFilter.slideUp();
+			}
+		});
+
+	/*-------------------------------------------------*/
+	/* =  flexslider
+	/*-------------------------------------------------*/
+	try {
+
+		var SliderPost = $('.flexslider');
+
+		SliderPost.flexslider({
+			animation: "fade"
+		});
+	} catch(err) {
+
+	}
+
+	/*-------------------------------------------------*/
+	/* =  Banner slider
+	/*-------------------------------------------------*/
+
+	var sliderTestimonial = $('.bxslider');
+	try{		
+		sliderTestimonial.bxSlider({
+			mode: 'vertical'
+		});
+	} catch(err) {
+	}
+
+	/* ---------------------------------------------------------------------- */
+	/*	Contact Map
+	/* ---------------------------------------------------------------------- */
+	var contact = {"lat":"52.204914", "lon":"0.121686"}; //Change a map coordinate here!
+
+	try {
+		var mapContainer = $('.map');
+		mapContainer.gmap3({
+			action: 'addMarker',
+			latLng: [contact.lat, contact.lon],
+			map:{
+				center: [contact.lat, contact.lon],
+				zoom: 14
+				},
+			},
+			{action: 'setOptions', args:[{scrollwheel:true}]}
+		);
+	} catch(err) {
+
+	}
+
+	/* ---------------------------------------------------------------------- */
+	/*	Contact Form
+	/* ---------------------------------------------------------------------- */
+
+	var submitContact = $('#submit_contact'),
+		message = $('#msg');
+
+	submitContact.on('click', function(e){
+		e.preventDefault();
+
+		var $this = $(this);
+		
+		$.ajax({
+			type: "POST",
+			url: 'contact.php',
+			dataType: 'json',
+			cache: false,
+			data: $('#contact-form').serialize(),
+			success: function(data) {
+
+				if(data.info !== 'error'){
+					$this.parents('form').find('input[type=text],textarea,select').filter(':visible').val('');
+					message.hide().removeClass('success').removeClass('error').addClass('success').html(data.msg).fadeIn('slow').delay(5000).fadeOut('slow');
+				} else {
+					message.hide().removeClass('success').removeClass('error').addClass('error').html(data.msg).fadeIn('slow').delay(5000).fadeOut('slow');
+				}
+			}
+		});
+	});
+
+
+	(function() {
+
+		var docElem = document.documentElement,
+			didScroll = false,
+			changeHeaderOn = 100;
+			document.querySelector( 'header' );
+		function init() {
+			window.addEventListener( 'scroll', function() {
+				if( !didScroll ) {
+					didScroll = true;
+					setTimeout( scrollPage, 250 );
+				}
+			}, false );
+		}
+		
+		function scrollPage() {
+			var sy = scrollY();
+			if ( sy >= changeHeaderOn ) {
+				$( '.top-line' ).slideUp(400);
+			}
+			else {
+				$( '.top-line' ).slideDown(400);
+			}
+			didScroll = false;
+		}
+		
+		function scrollY() {
+			return window.pageYOffset || docElem.scrollTop;
+		}
+		
+		init();
+		
+	})();
+
+});
